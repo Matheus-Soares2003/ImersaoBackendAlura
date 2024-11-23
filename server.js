344239609f7be03fe5c346bee531103ac66193c8
Matheus Cabral <matheusstcabral@gmail.com>
2024-11-22 23:29:58 -0300

    primeira rota

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ import express from 'express'
 const app = express(); //app representa o servidor
 
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+    console.log("Servidor escutando..."); //servidor vai escutar na porta 3000
+});
+
+//Espera uma requisição GET para /api
+app.get("/api", (req, res) => {
+    res.status(200).send("Bem-Vindo à minha API!");
 });
