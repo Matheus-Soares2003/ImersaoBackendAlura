40b0c674d72de20630d179fef4eeb23f9361acba
Matheus Cabral <matheusstcabral@gmail.com>
2024-11-23 00:03:35 -0300

    criacao da rota para pegar um livro pelo seu ID

--- server.js
+++ server.js
@@ -2,33 +2,63 @@ import express from 'express'
 
 const livros = [
     {
+      id: 1,
       titulo: "Harry Potter",
       autor: "J.K. Rowling",
       imagem: "https://placecats.com/300/200"
     },
     {
+      id: 2,
       titulo: "O Senhor dos Anéis",
       autor: "J.R.R. Tolkien",
       imagem: "https://placecats.com/300/200"
     },
     {
+      id: 3,
       titulo: "1984",
       autor: "George Orwell",
       imagem: "https://placecats.com/300/200"
     },
     {
+      id: 4,
       titulo: "O Pequeno Príncipe",
       autor: "Antoine de Saint-Exupéry",
       imagem: "https://placecats.com/300/200"
     },
     {
+      id: 5,
       titulo: "Orgulho e Preconceito",
       autor: "Jane Austen",
       imagem: "https://placecats.com/300/200"
     },
     {
+      id: 6,
       titulo: "Dom Quixote",
       autor: "Miguel de Cervantes",
+      imagem: "https://placecats.com/300/200"
+    },
+    {
+      id: 7,
+      titulo: "A Metamorfose",
+      autor: "Franz Kafka",
+      imagem: "https://placecats.com/300/200"
+    },
+    {
+      id: 8,
+      titulo: "O Nome da Rosa",
+      autor: "Umberto Eco",
+      imagem: "https://placecats.com/300/200"
+    },
+    {
+      id: 9,
+      titulo: "O Alquimista",
+      autor: "Paulo Coelho",
+      imagem: "https://placecats.com/300/200"
+    },
+    {
+      id: 10,
+      titulo: "Cem Anos de Solidão",
+      autor: "Gabriel García Márquez",
       imagem: "https://placecats.com/300/200"
     }
 ];
@@ -44,3 +74,16 @@ app.listen(3000, () => {
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
+
+//Devolve um livro baseado em seu ID (/livro/1)
+app.get("/livro/:id", (req, res) => {
+    const idx = buscarLivroPorID(req.params.id); //pega o parametro id passado pela url
+    res.status(200).json(livros[idx]);
+});
+
+
+function buscarLivroPorID(id) {
+    return livros.findIndex(livro =>  {
+        return livro.id === Number(id);
+    }); //Entra em cada objeto dentro do array, acha o livro pelo ID, e retorna o index onde esse livro está
+}
